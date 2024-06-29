11a8489cd6e5f3675e85f6d04a8a3b877eb48085
Kamlesh <81345800+kamleshhv@users.noreply.github.com>
2024-06-29 12:20:52 +0530

    blaze script

---
+++ blaze.sh
@@ -0,0 +1,8 @@
+mkdir blaze
+cd blaze
+repo init --depth=1 -u https://github.com/ProjectBlaze/manifest -b 14-QPR3
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
+git clone https://github.com/kamleshhv/device_xiaomi_sweet.git -b blaze device/xiaomi/sweet
+. b*/e*
+lunch blaze_sweet-userdebug
+make bacon -j8
