6590aafe21a46dda1fa21a898890e221ec192a06
Kamlesh <81345800+kamleshhv@users.noreply.github.com>
2024-08-18 17:25:47 +0530

    Update matrixx.sh

--- matrixx.sh
+++ matrixx.sh
@@ -2,5 +2,6 @@ mkdir matrixx
 cd matrixx
 repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
-git clone https://github.com/kamleshhv/device_xiaomi_sweet.git -b matrixx device/xiaomi/sweet
+git clone https://github.com/kamleshhv/device_xiaomi_sweet.git -b mat device/xiaomi/sweet
+git clone https://github.com/Matrixx-Devices/android_vendor_lineage-priv.git -b 14.0 vendor/lineage-priv
 . b*/e*
