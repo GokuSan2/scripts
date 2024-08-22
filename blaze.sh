64a3a9e3edf5499041d0b1704a4a7419e19a2cb8
Kamlesh <81345800+kamleshhv@users.noreply.github.com>
2024-08-22 16:04:25 +0530

    Update blaze.sh

--- blaze.sh
+++ blaze.sh
@@ -1,6 +1,6 @@
 mkdir blaze
 cd blaze
-repo init --depth=1 -u https://github.com/ProjectBlaze/manifest -b 14-QPR3
+repo init --depth=1 -u https://github.com/ProjectBlaze/manifest -b 14
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
 git clone https://github.com/sweet-source/device_xiaomi_sweet.git -b 14 device/xiaomi/sweet
 . b*/e*
