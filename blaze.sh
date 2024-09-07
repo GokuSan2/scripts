9fbb6f106bdcdd6066505b2f790d5162ae55e78a
Kamlesh <81345800+kamleshhv@users.noreply.github.com>
2024-09-07 17:43:13 +0530

    Update blaze.sh

--- blaze.sh
+++ blaze.sh
@@ -1,8 +1,13 @@
 mkdir blaze
 cd blaze
-repo init --depth=1 -u https://github.com/ProjectBlaze/manifest -b 14
+repo init -u https://github.com/ProjectBlaze/manifest -b 14
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
 git clone https://github.com/sweet-source/device_xiaomi_sweet.git -b 14 device/xiaomi/sweet
+mkdir prebuilts/clang/host/linux-x86/clang-neutron; cd prebuilts/clang/host/linux-x86/clang-neutron; 
+curl -LO "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman";
+chmod +x antman; ./antman -S=05012024; ./antman --patch=glibc; cd ../../../../../;
+sed -i '/CLANG_EXTRA_FLAGS += --hip-path=\/dev\/null/d' vendor/blaze/build/tasks/kernel.mk;
+git clone https://github.com/afterallafk/vendor_extra.git vendor/extra
 . b*/e*
 lunch blaze_sweet-ap2a-userdebug
-make bacon -j48
+make bacon -j$(nproc --all)
